8ca1362b713226715c03673d0878823aa2981b4c
shassathe <shsathe@health.ucsd.edu>
2020-05-10 07:56:59 -0700

    .

--- covid_qPCR_test/settings.py
+++ covid_qPCR_test/settings.py
@@ -24,7 +24,7 @@ SECRET_KEY = config('DJANGO_SECRET_KEY')
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['*', 'Inspect.us-west-1.elasticbeanstalk.com']
 
 # Application definition
 
